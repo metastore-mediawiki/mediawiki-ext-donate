af2b2d980f59684b42df6e1a96f7103471a80df9
Kitsune Solar <kitsune.solar@gmail.com>
2018-07-20 10:18:29 +0300

    2018-07-20 07:18:29

--- MW_EXT_Donate.class.php
+++ MW_EXT_Donate.class.php
@@ -3,7 +3,7 @@
 namespace MediaWiki\Extension\MW_EXT_Donate;
 
 use OutputPage, Parser, PPFrame, Skin;
-use MediaWiki\Extension\MW_EXT_Core\MW_EXT_Core;
+use MediaWiki\Extension\MW_EXT_Kernel\MW_EXT_Kernel;
 
 /**
  * Class MW_EXT_Donate
@@ -36,62 +36,62 @@ class MW_EXT_Donate {
 	 */
 	public static function onRenderTag( Parser $parser, PPFrame $frame, $args = [] ) {
 		// Get options parser.
-		$getOption = MW_EXT_Core::extractOptions( $args, $frame );
+		$getOption = MW_EXT_Kernel::extractOptions( $args, $frame );
 
 		// Argument: receiver.
-		$getWallet = MW_EXT_Core::outClear( $getOption['ya-wallet'] ?? '' ?: '' );
+		$getWallet = MW_EXT_Kernel::outClear( $getOption['ya-wallet'] ?? '' ?: '' );
 		$outWallet = $getWallet;
 
 		// Argument: fio.
-		$getFIO = MW_EXT_Core::outClear( $getOption['ya-fio'] ?? '' ?: '' );
+		$getFIO = MW_EXT_Kernel::outClear( $getOption['ya-fio'] ?? '' ?: '' );
 		$outFIO = $getFIO;
 
 		// Argument: email.
-		$getEmail = MW_EXT_Core::outClear( $getOption['ya-email'] ?? '' ?: '' );
+		$getEmail = MW_EXT_Kernel::outClear( $getOption['ya-email'] ?? '' ?: '' );
 		$outEmail = $getEmail;
 
 		// Argument: phone.
-		$getPhone = MW_EXT_Core::outClear( $getOption['ya-phone'] ?? '' ?: '' );
+		$getPhone = MW_EXT_Kernel::outClear( $getOption['ya-phone'] ?? '' ?: '' );
 		$outPhone = $getPhone;
 
 		// Argument: address.
-		$getAddress = MW_EXT_Core::outClear( $getOption['ya-address'] ?? '' ?: '' );
+		$getAddress = MW_EXT_Kernel::outClear( $getOption['ya-address'] ?? '' ?: '' );
 		$outAddress = $getAddress;
 
 		// Argument: target.
-		$getTargets = MW_EXT_Core::outClear( $getOption['ya-target'] ?? '' ?: MW_EXT_Core::getMessageText( 'donate', 'targets' ) );
+		$getTargets = MW_EXT_Kernel::outClear( $getOption['ya-target'] ?? '' ?: MW_EXT_Kernel::getMessageText( 'donate', 'targets' ) );
 		$outTargets = $getTargets;
 
 		// Argument: sum.
-		$getSum = MW_EXT_Core::outClear( $getOption['ya-sum'] ?? '' ?: '100' );
+		$getSum = MW_EXT_Kernel::outClear( $getOption['ya-sum'] ?? '' ?: '100' );
 		$outSum = $getSum;
 
 		// Get form comment.
-		$getFormComment = MW_EXT_Core::outClear( MW_EXT_Core::getConfig( 'Sitename' ) ) . ': ' . MW_EXT_Core::outClear( MW_EXT_Core::getTitle()->getText() );
+		$getFormComment = MW_EXT_Kernel::outClear( MW_EXT_Kernel::getConfig( 'Sitename' ) ) . ': ' . MW_EXT_Kernel::outClear( MW_EXT_Kernel::getTitle()->getText() );
 		$outFormComment = $getFormComment;
 
 		// Get short destination.
-		$getShortDest = MW_EXT_Core::outClear( MW_EXT_Core::getConfig( 'Sitename' ) ) . ': ' . MW_EXT_Core::outClear( MW_EXT_Core::getTitle()->getText() );
+		$getShortDest = MW_EXT_Kernel::outClear( MW_EXT_Kernel::getConfig( 'Sitename' ) ) . ': ' . MW_EXT_Kernel::outClear( MW_EXT_Kernel::getTitle()->getText() );
 		$outShortDest = $getShortDest;
 
 		// Argument: card.
-		$getCard = MW_EXT_Core::outClear( $getOption['card'] ?? '' ?: '' );
+		$getCard = MW_EXT_Kernel::outClear( $getOption['card'] ?? '' ?: '' );
 		$outCard = $getCard;
 
 		// Argument: paypal.
-		$getPayPal = MW_EXT_Core::outClear( $getOption['paypal'] ?? '' ?: '' );
+		$getPayPal = MW_EXT_Kernel::outClear( $getOption['paypal'] ?? '' ?: '' );
 		$outPayPal = $getPayPal;
 
 		// Argument: bitcoin.
-		$getBitcoin = MW_EXT_Core::outClear( $getOption['bitcoin'] ?? '' ?: '' );
+		$getBitcoin = MW_EXT_Kernel::outClear( $getOption['bitcoin'] ?? '' ?: '' );
 		$outBitcoin = $getBitcoin;
 
 		// Argument: ethereum.
-		$getEthereum = MW_EXT_Core::outClear( $getOption['ethereum'] ?? '' ?: '' );
+		$getEthereum = MW_EXT_Kernel::outClear( $getOption['ethereum'] ?? '' ?: '' );
 		$outEthereum = $getEthereum;
 
 		// Argument: monero.
-		$getMonero = MW_EXT_Core::outClear( $getOption['monero'] ?? '' ?: '' );
+		$getMonero = MW_EXT_Kernel::outClear( $getOption['monero'] ?? '' ?: '' );
 		$outMonero = $getMonero;
 
 		// Out HTML.
@@ -102,7 +102,7 @@ class MW_EXT_Donate {
 			$outHTML .= '<div class="mw-ext-donate-body">';
 			$outHTML .= '<div class="mw-ext-donate-icon"><div><i class="fab fa-yandex-international"></i></div></div>';
 			$outHTML .= '<div class="mw-ext-donate-content">';
-			$outHTML .= '<div class="mw-ext-donate-title">' . MW_EXT_Core::getMessageText( 'donate', 'to-yandex' ) . '</div>';
+			$outHTML .= '<div class="mw-ext-donate-title">' . MW_EXT_Kernel::getMessageText( 'donate', 'to-yandex' ) . '</div>';
 			$outHTML .= '<form method="POST" action="https://money.yandex.ru/quickpay/confirm.xml">';
 			$outHTML .= '<input name="receiver" value="' . $outWallet . '" type="hidden" />';
 			$outHTML .= '<input name="formcomment" value="' . $outFormComment . '" type="hidden" />';
@@ -128,17 +128,17 @@ class MW_EXT_Donate {
 			}
 
 			$outHTML .= '<div class="mw-ext-donate-form">';
-			$outHTML .= '<div><div><input name="sum" value="' . $outSum . '" data-type="number" type="number" placeholder="' . MW_EXT_Core::getMessageText( 'donate', 'sum-placeholder' ) . '" /></div>';
-			$outHTML .= '<div class="mw-ext-donate-desc">' . MW_EXT_Core::getMessageText( 'donate', 'to-yandex-desc-sum' ) . '</div></div>';
-			$outHTML .= '<div><div><textarea name="comment" placeholder="' . MW_EXT_Core::getMessageText( 'donate', 'comment-placeholder' ) . '"></textarea></div>';
-			$outHTML .= '<div class="mw-ext-donate-desc">' . MW_EXT_Core::getMessageText( 'donate', 'to-yandex-desc-comment' ) . '</div></div>';
+			$outHTML .= '<div><div><input name="sum" value="' . $outSum . '" data-type="number" type="number" placeholder="' . MW_EXT_Kernel::getMessageText( 'donate', 'sum-placeholder' ) . '" /></div>';
+			$outHTML .= '<div class="mw-ext-donate-desc">' . MW_EXT_Kernel::getMessageText( 'donate', 'to-yandex-desc-sum' ) . '</div></div>';
+			$outHTML .= '<div><div><textarea name="comment" placeholder="' . MW_EXT_Kernel::getMessageText( 'donate', 'comment-placeholder' ) . '"></textarea></div>';
+			$outHTML .= '<div class="mw-ext-donate-desc">' . MW_EXT_Kernel::getMessageText( 'donate', 'to-yandex-desc-comment' ) . '</div></div>';
 			$outHTML .= '</div><div class="mw-ext-donate-select"><div>';
-			$outHTML .= '<div><input id="paymentTypePC" name="paymentType" value="PC" type="radio" /><label title="' . MW_EXT_Core::getMessageText( 'donate', 'payment-pc' ) . '" for="paymentTypePC"><i class="fab fa-yandex-international fa-fw"></i></label></div>';
-			$outHTML .= '<div><input id="paymentTypeAC" name="paymentType" value="AC" type="radio" checked /><label title="' . MW_EXT_Core::getMessageText( 'donate', 'payment-ac' ) . '" for="paymentTypeAC"><i class="far fa-credit-card fa-fw"></i></label></div>';
-			$outHTML .= '<div><input id="paymentTypeMC" name="paymentType" value="MC" type="radio" /><label title="' . MW_EXT_Core::getMessageText( 'donate', 'payment-mc' ) . '" for="paymentTypeMC"><i class="fas fa-mobile-alt fa-fw"></i></label></div>';
-			$outHTML .= '</div><div class="mw-ext-donate-desc">' . MW_EXT_Core::getMessageText( 'donate', 'to-yandex-desc-payment-type' ) . '</div>';
+			$outHTML .= '<div><input id="paymentTypePC" name="paymentType" value="PC" type="radio" /><label title="' . MW_EXT_Kernel::getMessageText( 'donate', 'payment-pc' ) . '" for="paymentTypePC"><i class="fab fa-yandex-international fa-fw"></i></label></div>';
+			$outHTML .= '<div><input id="paymentTypeAC" name="paymentType" value="AC" type="radio" checked /><label title="' . MW_EXT_Kernel::getMessageText( 'donate', 'payment-ac' ) . '" for="paymentTypeAC"><i class="far fa-credit-card fa-fw"></i></label></div>';
+			$outHTML .= '<div><input id="paymentTypeMC" name="paymentType" value="MC" type="radio" /><label title="' . MW_EXT_Kernel::getMessageText( 'donate', 'payment-mc' ) . '" for="paymentTypeMC"><i class="fas fa-mobile-alt fa-fw"></i></label></div>';
+			$outHTML .= '</div><div class="mw-ext-donate-desc">' . MW_EXT_Kernel::getMessageText( 'donate', 'to-yandex-desc-payment-type' ) . '</div>';
 			$outHTML .= '</div><div class="mw-ext-donate-submit">';
-			$outHTML .= '<input class="mw-ext-donate-button" value="' . MW_EXT_Core::getMessageText( 'donate', 'submit' ) . '" type="submit" />';
+			$outHTML .= '<input class="mw-ext-donate-button" value="' . MW_EXT_Kernel::getMessageText( 'donate', 'submit' ) . '" type="submit" />';
 			$outHTML .= '</div></form></div></div>';
 		}
 
@@ -147,9 +147,9 @@ class MW_EXT_Donate {
 			$outHTML .= '<div class="mw-ext-donate-body">';
 			$outHTML .= '<div class="mw-ext-donate-icon"><div><i class="fas fa-credit-card"></i></div></div>';
 			$outHTML .= '<div class="mw-ext-donate-content">';
-			$outHTML .= '<div class="mw-ext-donate-title">' . MW_EXT_Core::getMessageText( 'donate', 'to-card' ) . '</div>';
+			$outHTML .= '<div class="mw-ext-donate-title">' . MW_EXT_Kernel::getMessageText( 'donate', 'to-card' ) . '</div>';
 			$outHTML .= '<div><input value="' . $outCard . '" type="text" readonly /></div>';
-			$outHTML .= '<div class="mw-ext-donate-desc">' . MW_EXT_Core::getMessageText( 'donate', 'to-card-desc' ) . '</div>';
+			$outHTML .= '<div class="mw-ext-donate-desc">' . MW_EXT_Kernel::getMessageText( 'donate', 'to-card-desc' ) . '</div>';
 			$outHTML .= '</div></div>';
 		}
 
@@ -158,9 +158,9 @@ class MW_EXT_Donate {
 			$outHTML .= '<div class="mw-ext-donate-body">';
 			$outHTML .= '<div class="mw-ext-donate-icon"><div><i class="fab fa-paypal"></i></div></div>';
 			$outHTML .= '<div class="mw-ext-donate-content">';
-			$outHTML .= '<div class="mw-ext-donate-title">' . MW_EXT_Core::getMessageText( 'donate', 'to-paypal' ) . '</div>';
+			$outHTML .= '<div class="mw-ext-donate-title">' . MW_EXT_Kernel::getMessageText( 'donate', 'to-paypal' ) . '</div>';
 			$outHTML .= '<div><a class="mw-ext-donate-button" href="https://www.paypal.me/' . $outPayPal . '" rel="nofollow" target="_blank">' . $outPayPal . '</a></div>';
-			$outHTML .= '<div class="mw-ext-donate-desc">' . MW_EXT_Core::getMessageText( 'donate', 'to-paypal-desc' ) . '</div>';
+			$outHTML .= '<div class="mw-ext-donate-desc">' . MW_EXT_Kernel::getMessageText( 'donate', 'to-paypal-desc' ) . '</div>';
 			$outHTML .= '</div></div>';
 		}
 
@@ -169,9 +169,9 @@ class MW_EXT_Donate {
 			$outHTML .= '<div class="mw-ext-donate-body">';
 			$outHTML .= '<div class="mw-ext-donate-icon"><div><i class="fab fa-bitcoin"></i></div></div>';
 			$outHTML .= '<div class="mw-ext-donate-content">';
-			$outHTML .= '<div class="mw-ext-donate-title">' . MW_EXT_Core::getMessageText( 'donate', 'to-bitcoin' ) . '</div>';
+			$outHTML .= '<div class="mw-ext-donate-title">' . MW_EXT_Kernel::getMessageText( 'donate', 'to-bitcoin' ) . '</div>';
 			$outHTML .= '<div><input value="' . $outBitcoin . '" type="text" readonly /></div>';
-			$outHTML .= '<div class="mw-ext-donate-desc">' . MW_EXT_Core::getMessageText( 'donate', 'to-bitcoin-desc' ) . '</div>';
+			$outHTML .= '<div class="mw-ext-donate-desc">' . MW_EXT_Kernel::getMessageText( 'donate', 'to-bitcoin-desc' ) . '</div>';
 			$outHTML .= '</div></div>';
 		}
 
@@ -180,9 +180,9 @@ class MW_EXT_Donate {
 			$outHTML .= '<div class="mw-ext-donate-body">';
 			$outHTML .= '<div class="mw-ext-donate-icon"><div><i class="fab fa-ethereum"></i></div></div>';
 			$outHTML .= '<div class="mw-ext-donate-content">';
-			$outHTML .= '<div class="mw-ext-donate-title">' . MW_EXT_Core::getMessageText( 'donate', 'to-ethereum' ) . '</div>';
+			$outHTML .= '<div class="mw-ext-donate-title">' . MW_EXT_Kernel::getMessageText( 'donate', 'to-ethereum' ) . '</div>';
 			$outHTML .= '<div><input value="' . $outEthereum . '" type="text" readonly /></div>';
-			$outHTML .= '<div class="mw-ext-donate-desc">' . MW_EXT_Core::getMessageText( 'donate', 'to-ethereum-desc' ) . '</div>';
+			$outHTML .= '<div class="mw-ext-donate-desc">' . MW_EXT_Kernel::getMessageText( 'donate', 'to-ethereum-desc' ) . '</div>';
 			$outHTML .= '</div></div>';
 		}
 
@@ -191,9 +191,9 @@ class MW_EXT_Donate {
 			$outHTML .= '<div class="mw-ext-donate-body">';
 			$outHTML .= '<div class="mw-ext-donate-icon"><div><i class="fab fa-monero"></i></div></div>';
 			$outHTML .= '<div class="mw-ext-donate-content">';
-			$outHTML .= '<div class="mw-ext-donate-title">' . MW_EXT_Core::getMessageText( 'donate', 'to-monero' ) . '</div>';
+			$outHTML .= '<div class="mw-ext-donate-title">' . MW_EXT_Kernel::getMessageText( 'donate', 'to-monero' ) . '</div>';
 			$outHTML .= '<div><input value="' . $outMonero . '" type="text" readonly /></div>';
-			$outHTML .= '<div class="mw-ext-donate-desc">' . MW_EXT_Core::getMessageText( 'donate', 'to-monero-desc' ) . '</div>';
+			$outHTML .= '<div class="mw-ext-donate-desc">' . MW_EXT_Kernel::getMessageText( 'donate', 'to-monero-desc' ) . '</div>';
 			$outHTML .= '</div></div>';
 		}
 
